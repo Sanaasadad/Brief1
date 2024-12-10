0d109754c20c7a8aefca48c25d48958fffb90dde
Sanaasadad <sanaasadad4@gmail.com>
2024-12-10 12:20:33 +0100

    test de code

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner Input = new Scanner(System.in);
+        System.out.print("Enter number 1 :");
+        int n1 = Input.nextInt();
+        System.out.print("Enter number 2 :");
+        int n2 = Input.nextInt();
+        Sys
+
+
+    }
+
+
+
+}
