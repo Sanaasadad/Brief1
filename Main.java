e9d80a3fefe30e4ac78467063c74b9e22b85dd0a
Sanaasadad <sanaasadad4@gmail.com>
2024-12-11 11:02:53 +0100

    fonction substract

--- Main.java
+++ Main.java
@@ -2,36 +2,39 @@
 import java.util.Scanner;
 
 public class Main {
-    static int n1,n2;
+    static int n1,n2,i;
     static void Addition (){
         System.out.println(n1 + " + " + n2 + " is: " + (n1 + n2));
     }
-
+    static void Subtract () {
+        System.out.println(n1 + " - " + n2 + " is: " + (n1 - n2));
+    }
         public static void main( String [] args) {
         int choice ;
-        System.out.println("1-Addition\n 2-Subtract\n 3-Multiplication\n 4-Division\n 5-power\n 6-Square\n 7-Factorial\n  ");
-        System.out.println("choose an option: ");
-        Scanner SS = new Scanner(System.in);
-        choice = SS.nextInt();
-        System.out.println("Entre first number: ");
-        n1 = SS.nextInt();
-        System.out.println("Entre second number: ");
-        n2 = SS.nextInt();
-            switch(choice){
-            case 1:
-                Addition();
-                break;
-            case 2:
-                System.out.println(" try again ");
-                break;
-            default:
-                System.out.println("the choice is invalid");
+        do {
+            System.out.println("1-Addition\n 2-Subtract\n 3-Multiplication\n 4-Division\n 5-power\n 6-Square\n 7-Factorial\n  ");
+            System.out.println("choose an option: ");
+            Scanner SS = new Scanner(System.in);
+            choice = SS.nextInt();
+            System.out.println("Entre first number: ");
+            n1 = SS.nextInt();
+            System.out.println("Entre second number: ");
+            n2 = SS.nextInt();
 
-
-
-
-
-        }
+            switch(choice) {
+                case 1:
+                    Addition();
+                    break;
+                case 2:
+                    Subtract();
+                    break;
+                case 3:
+                    System.out.println(" try again ");
+                    break;
+                default:
+                    System.out.println("the choice is invalid");
+            }
+        } while (choice!=0);
 
 
     }
